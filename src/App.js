de984b46f225d3b247d85fc3d9d99238f5ee4d05
asavienko <asavienko@gmail.com>
2019-02-12 21:11:24 +0200

    changes from Bohdan (#1)
    * changes from Bohdan
    
    * made changes for review. Update gitignore

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
 import InputComponent from "./inputComponent";
-import ShowComponent from "./showComponent";
+import ListStyleComponent from "./listStyleComponent";
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {arrayOfElements: ["asd", "adasda"]};
+    this.state = {arrayOfElements: []};
 
   }
 
   addElement = (value) => {
-    this.state.arrayOfElements.push(value);
-    console.log(this.state.arrayOfElements)
-    //possible todo
-    //const newArray = this.state.arrayOfElements.concat(value);
-    //this.setState({arrayOfElements: newArray});
-    //
-  };
-
-  showElements = ({arrayOfElements: array}) => {
-    const elements = array.length > 0 && array.map((element, index) => {
-        return (<li key={index.toString()}>{element}
-          <button>Delete</button>
-        </li>)
-      }
-    );
-    return elements;
+    const newArrayOfToDos = this.state.arrayOfElements.concat(value);
+    this.setState({arrayOfElements: newArrayOfToDos});
   };
 
   render() {
-    //debug what you have here in this.state
+    const {arrayOfElements} = this.state;
     return (
       <div className="App">
         <InputComponent addElement={this.addElement.bind(this)}/>
-        <ul>{this.showElements(this.state)}</ul>
+        <ul>
+          {arrayOfElements.length > 0 && arrayOfElements.map((element, index) => {
+              return (
+                  <ListStyleComponent element={element} index={index}/>
+              )}
+          )}
+        </ul>
       </div>
     );
   }
