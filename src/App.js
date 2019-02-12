5c889ea39edb4149756127855e109cfcb647455d
Anton <asavienko@gmail.com>
2019-02-12 10:27:51 +0200

    I fixed addElement function and all work. And installed material-ui

--- src/App.js
+++ src/App.js
@@ -7,35 +7,28 @@ import ShowComponent from "./showComponent";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {arrayOfElements: ["asd", "adasda"]};
+    this.state = {arrayOfElements: []};
 
   }
 
   addElement = (value) => {
-    this.state.arrayOfElements.push(value);
-    console.log(this.state.arrayOfElements)
-    //possible todo
-    //const newArray = this.state.arrayOfElements.concat(value);
-    //this.setState({arrayOfElements: newArray});
-    //
+    const newArray = this.state.arrayOfElements.concat(value);
+    this.setState({arrayOfElements: newArray});
   };
 
-  showElements = ({arrayOfElements: array}) => {
-    const elements = array.length > 0 && array.map((element, index) => {
-        return (<li key={index.toString()}>{element}
-          <button>Delete</button>
-        </li>)
-      }
-    );
-    return elements;
+  deleteElement = (id) => {
+    const newArray = this.state.arrayOfElements.filter((element, index) => id !== index);
+    this.setState({arrayOfElements: newArray});
+
   };
+
 
   render() {
-    //debug what you have here in this.state
+    console.log(this.state)
     return (
       <div className="App">
         <InputComponent addElement={this.addElement.bind(this)}/>
-        <ul>{this.showElements(this.state)}</ul>
+        <ShowComponent array={this.state} delete={this.deleteElement.bind(this)}/>
       </div>
     );
   }
--- src/showComponent.js
+++ src/showComponent.js
@@ -2,11 +2,19 @@ import React from "react"
 
 class ShowComponent extends React.Component {
 
-
+  showElements = ({arrayOfElements: array}) => {
+    const elements = array.length > 0 && array.map((element, index) => {
+        return (<li key={index.toString()}>{element}
+          <button onClick={()=> this.props.delete(index)}>Delete</button>
+        </li>)
+      }
+    );
+    return elements;
+  };
 
 
   render() {
-    return <ul>{this.showElements}</ul>
+    return <ul>{this.showElements(this.props.array)}</ul>
   }
 }
 
