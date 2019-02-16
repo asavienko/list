9b4a4099dee4c04ea60dddf4595268aa6467a1e4
Anton <asavienko@gmail.com>
2019-02-16 10:09:23 +0200

    create new branch

--- src/listStyleComponent.js
+++ src/listStyleComponent.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import ListItem from "@material-ui/core/ListItem";
 
 const ListStyleComponent = ({index, element}) => {
-  return <li key={index}>
-    {element}
-    <button>Delete</button>
-  </li>
+  return <ListItem key={index.toString()} dense button onClick={this.handleToggle(element)}>
+    <Checkbox
+      checked={this.state.checked.indexOf(element) !== -1}
+      tabIndex={-1}
+      disableRipple
+    />
+    <ListItemText primary={element}/>
+    <ListItemSecondaryAction>
+      <IconButton aria-label="Delete" onClick={() => this.props.delete(index)}>
+
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>
 };
 
 export default ListStyleComponent
