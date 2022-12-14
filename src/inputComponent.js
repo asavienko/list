302e0e3f138d2f9ef0b9bc9a492b0763ea5210fd
Anton Savienko <asavienko@gmail.com>
2022-12-14 11:43:44 -0700

    modified inputComponent (#2)

--- src/inputComponent.js
+++ src/inputComponent.js
@@ -1,9 +1,35 @@
 import React from "react"
+import {Divider, IconButton, InputBase, Paper} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+
+
+const styles = {
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+    margin: 20,
+    zIndex: 10,
+  },
+  input: {
+    width: "100%"
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
+  },
+  button: {
+    padding: 10,
+  },
+};
+
 
 class InputComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ""}
+    this.state = {value: ""};
   }
 
   onSubmit = () => {
@@ -16,10 +42,21 @@ class InputComponent extends React.Component {
   };
 
   render() {
-    return <React.Fragment>
-      <input onChange={this.handleInputChange} id={"submit"} value={this.state.value}/>
-      <button onClick={()=>this.onSubmit()}>Submit</button>
-    </React.Fragment>
+    return <Paper style={styles.root} elevation={1}>
+      <InputBase
+        style={styles.input}
+        autoComplete='off'
+        placeholder={"Введите заметку"}
+        onChange={this.handleInputChange}
+        id={"submit"}
+        value={this.state.value}
+      />
+      <Divider style={styles.divider}/>
+      <IconButton style={styles.button} onClick={() => this.onSubmit()}>
+        <AddIcon color="secondary"/>
+      </IconButton>
+    </Paper>
+
   }
 }
 
